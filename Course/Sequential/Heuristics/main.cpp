18e449a39ee0352e1c56a0259a9a0ea775664d55
CEDipEngineering <cadudip@hotmail.com>
2022-03-16 12:33:55 -0300

    Template start for all algorithms;

--- Course/Sequential/Heuristics/main.cpp
+++ Course/Sequential/Heuristics/main.cpp
@@ -1,5 +1,38 @@
 #include <iostream>
+#include<vector>
+#include<algorithm>
+#include<random>
+
+struct Item {
+    int id;
+    int weight;
+    int value;
+};
+
+struct Knapsack {
+    int id;
+    int weight;
+    bool arr[];
+};
 
 int main(){
-    std::cout << "Hello there!";
+    // Read number of items and capacity of knapsacks
+    int n = 0;
+    int W = 0;
+    std::cin >> n >> W;
+
+    // Read all items
+    std::vector<Item> items;
+    items.reserve(n);
+    int w,v;
+    for(int i = 0; i<n; i++){
+        std::cin >> w >> v;
+        items.push_back({i, w, v});
+    }
+
+    // Algorithm
+
+    // Print output
+    
+    return 0;
 }
