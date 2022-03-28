179f84d365dd2a139da919ea2aba233548b331e5
CEDipEngineering <cadudip@hotmail.com>
2022-03-28 09:37:46 -0300

    Finished DNAProject LocalSearch

---
+++ DNAProject/LocalSearch/main.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <random>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+#define MATCH 2
+#define MISMATCH -1
+#define GAP -1
+
+int match_score(std::string sb, std::string a, int start, int subseq_size){
+    int score = 0;
+    char sa;
+    for(int i = 0; i<subseq_size; i++){
+        sa = a[i+start];
+        if (sa == sb[i]){
+            score += MATCH;
+        } else if (sa == '-' || sb[i] == '-') {
+            score += MISMATCH;
+        } else {
+            score += GAP;
+        }
+    }
+    return score;
+}
+
+int main(){
+    int len_a, len_b;
+    std::cin >> len_a >> len_b;
+    std::string a, b;
+    std::cin >> a >> b;
+
+    std::cout << "Valores lidos:" << std::endl << len_a << ": " << a << std::endl;
+    std::cout << len_b << ": " << b << std::endl;
+
+    std::default_random_engine generator(1337);
+    std::uniform_int_distribution<int> subseq_size_distro(4,len_b);
+
+    const int subseq_size = subseq_size_distro(generator);
+    std::cout << "Subsequence size: " << subseq_size << std::endl;
+    std::uniform_int_distribution<int> index_distro(0,len_b-subseq_size);
+
+    int i = index_distro(generator);
+    std::string sb;
+    for(int j = 0; j<subseq_size; j++){
+        sb.push_back(b[i+j]);
+    }
+
+    std::uniform_int_distribution<int> p_distro(10,100);
+    const int p = p_distro(generator);
+
+    int max_score = std::max(len_a, len_b)*std::max(MISMATCH, GAP); // Pior score possível é tudo em MISMATCH/GAP, dependendo dos pesos
+    int ix = 0;
+    int best_ix = 0;
+    for (int i = 0; i<p; i++){
+        ix = index_distro(generator);
+        int score = match_score(sb, a, ix, subseq_size);
+        if (score > max_score){
+            max_score = score;
+            best_ix = ix;
+        }
+    }
+
+    std::cout << "Sequências: " << std::endl;
+    std::cout << sb << std::endl;
+    for(int j = 0; j<subseq_size; j++){
+        std::cout << a[best_ix+j];
+    } 
+    std::cout << std::endl << "Score: " << max_score << std::endl;
+
+    return 0;
+}
