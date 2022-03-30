3bf009110b77fd993d57d515c6ca77bee61f88a3
CEDipEngineering <cadudip@hotmail.com>
2022-03-30 14:34:11 -0300

    Finished LocalSearch DNAProject

--- DNAProject/LocalSearch/main.cpp
+++ DNAProject/LocalSearch/main.cpp
@@ -25,42 +25,60 @@ int match_score(std::string sb, std::string a, int start, int subseq_size){
 }
 
 int main(){
+
+    // Inputs
     int len_a, len_b;
     std::cin >> len_a >> len_b;
     std::string a, b;
     std::cin >> a >> b;
 
-    std::cout << "Valores lidos:" << std::endl << len_a << ": " << a << std::endl;
-    std::cout << len_b << ": " << b << std::endl;
-
+    // RNG
     std::default_random_engine generator(1337);
     std::uniform_int_distribution<int> subseq_size_distro(4,len_b);
-
     const int subseq_size = subseq_size_distro(generator);
-    std::cout << "Subsequence size: " << subseq_size << std::endl;
-    std::uniform_int_distribution<int> index_distro(0,len_b-subseq_size);
-
-    int i = index_distro(generator);
-    std::string sb;
-    for(int j = 0; j<subseq_size; j++){
-        sb.push_back(b[i+j]);
-    }
-
+    
     std::uniform_int_distribution<int> p_distro(10,100);
     const int p = p_distro(generator);
 
-    int max_score = std::max(len_a, len_b)*std::max(MISMATCH, GAP); // Pior score possível é tudo em MISMATCH/GAP, dependendo dos pesos
-    int ix = 0;
-    int best_ix = 0;
-    for (int i = 0; i<p; i++){
-        ix = index_distro(generator);
-        int score = match_score(sb, a, ix, subseq_size);
-        if (score > max_score){
-            max_score = score;
-            best_ix = ix;
+    // Algorithm
+        // Ensure the largest string is a
+        int temp_len;
+        std::string temp;
+        if (len_a < len_b){
+            temp_len = len_b;
+            temp = b;
+            len_b = len_a;
+            b = a;
+            len_a = temp_len;
+            a = b;
         }
-    }
 
+        // Random subsize
+        std::cout << "Subsequence size: " << subseq_size << std::endl;
+        std::uniform_int_distribution<int> index_distro(0,len_b-subseq_size);
+
+        // Assemble substring b
+        int i = index_distro(generator);
+        std::string sb;
+        for(int j = 0; j<subseq_size; j++){
+            sb.push_back(b[i+j]);
+        }
+
+        // Try p combinations
+        int max_score = std::max(len_a, len_b)*std::max(MISMATCH, GAP); // Pior score possível é tudo em MISMATCH/GAP, dependendo dos pesos
+        int ix = 0;
+        int best_ix = 0;
+        for (int i = 0; i<p; i++){
+            ix = index_distro(generator);
+            int score = match_score(sb, a, ix, subseq_size);
+            if (score > max_score){
+                max_score = score;
+                best_ix = ix;
+            }
+        }
+
+
+    // Output
     std::cout << "Sequências: " << std::endl;
     std::cout << sb << std::endl;
     for(int j = 0; j<subseq_size; j++){
