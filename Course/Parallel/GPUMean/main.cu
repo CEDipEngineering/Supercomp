1c425ad3d98573dbccadd73c6d067c37460013d5
CEDipEngineering_VMSuperComp <cadudip@hotmail.com>
2022-05-13 18:24:41 +0000

    GPU stocks codes.

--- Course/Parallel/GPUMean/main.cu
+++ Course/Parallel/GPUMean/main.cu
@@ -3,43 +3,30 @@
 #include <thrust/functional.h>
 #include <thrust/transform.h>
 #include <iostream>
+#include <fstream>
 
 int main() {
-    thrust::device_vector<double> V1(10, 0);
-    thrust::sequence(V1.begin(), V1.end());
-
-    thrust::device_vector<double> V2(10, 0);
-    thrust::fill(V2.begin(), V2.begin() + 5, 5.5);
-    thrust::fill(V2.begin()+5, V2.end(), 10);
-
-    thrust::device_vector<double> V3(10);
-    thrust::device_vector<double> V4(10);
-
-    thrust::transform(V1.begin(), V1.end(), V2.begin(), V3.begin(), thrust::plus<double>());
-    thrust::transform(V1.begin(), V1.end(), thrust::constant_iterator<double>(0.5), V4.begin(), thrust::multiplies<double>());
-
-    printf("V1: ");
-    for (thrust::device_vector<double>::iterator i = V1.begin(); i != V1.end(); i++) {
-        std::cout << *i << " ";
+    std::vector<float> diff;
+    std::ifstream StockFile("./stocks2.csv");
+    std::string line;
+    while(std::getline(StockFile, line)){
+        int i = 0;
+        for(char c: line){
+            if (c == ' ') break;
+            i++;
+        }
+        float appl, micr;
+        appl = std::stof(line.substr(0, i));
+        micr = std::stof(line.substr(i, line.size()-i));
+        diff.push_back(appl-micr);
     }
-    printf("\n");
 
-    printf("V2: ");
-    for (thrust::device_vector<double>::iterator i = V2.begin(); i != V2.end(); i++) {
-        std::cout << *i << " ";
-    }
-    printf("\n");
+    const double inv_n = 1.0/diff.size();
 
-    printf("V3: ");
-    for (thrust::device_vector<double>::iterator i = V3.begin(); i != V3.end(); i++) {
-        std::cout << *i << " ";
-    }
-    printf("\n");
+    thrust::device_vector<float> V(diff);
+    float sum = thrust::reduce(V.begin(), V.end());
 
-    printf("V4: ");
-    for (thrust::device_vector<double>::iterator i = V4.begin(); i != V4.end(); i++) {
-        std::cout << *i << " ";
-    }
-    printf("\n");
+    const double mean = inv_n * sum;
 
+    std::cout << "Mean: " << mean << std::endl;
 }
